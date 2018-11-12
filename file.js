f465552d1f96b7729d270f9ef0f8ba5f6f590632
0gjimenez <42749723+0gjimenez@users.noreply.github.com>
2018-11-12 13:56:22 -0600

    Add files via upload

--- file.js
+++ file.js
@@ -2,16 +2,24 @@
 function TorrentFile(){
 
 //first function
+    function numberOfPackets(kB)
+
+        return kb/50
+
+
 
 
 
 
 
 //second function
+    function buildTorrentPacketArray(numOfPackets)
+        let list = []
+        let numOfPackets;
 
-
-
-
+            for(let i = 20; i < numOfPackets, i++ )
+                list [i] = 
 }
 
 //main test methods
+    console.log
