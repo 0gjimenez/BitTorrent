e0a699cc2a1cacd3f01778d8b07c6e8d0c2e8c28
0gjimenez <42749723+0gjimenez@users.noreply.github.com>
2018-11-15 13:56:53 -0600

    Add files via upload

--- tracker.js
+++ tracker.js
@@ -1,12 +1,30 @@
 //Type your code in this function
 function Tracker(){
 
-  let seeds =[];
+  let seeds = [];
   let percent = [];
 
   //implement all six functions below
+function numOfSeeds(list,value){
 
+  let found = 0
 
-
-
+  for(let i = 0; i < list.length; i++){
+  }
+    if(list [] === value){
+      
+      found++
+    }
+    return found
 }
+
+function addSeeds("s"){
+
+  let name = 
+ 
+  list = []
+
+for(i = 0;  > i)
+}
+
+
