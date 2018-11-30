9e919e943dabc4fea0851bd04eafdded0355bb3e
0gjimenez <42749723+0gjimenez@users.noreply.github.com>
2018-11-30 13:59:35 -0600

    Add files via upload

--- user.js
+++ user.js
@@ -2,25 +2,68 @@
 function User(tracker, torrent){
 
   let myTracker = tracker;
-  let myTorrent = torrent;
-
-  //Type all of your code in here.
-
- function User(tracker, torrent){
-
-  let myTracker = tracker;
-  
+ 
   let myTorrent = torrent;
 
   let state = 'leech'
   
-  let torrentFile = 0
+  let downloadSpeed = 0
 
-  let torrentTrackerFile = seeds
+  let uploadSpeed = 0
+  
+  //Type all of your code in here. 
 
-  let downloadSpeed = 17
+  function startDownload(){}
 
-  let unploadSpeed = 7
+  function updateTracker(){}
+  
+  function checkProgress(){}
+
+    function range(list){
+    
+     let min = findMin(list);
+     let max = findMax(List);
+
+      return [min, max]
+  }
+    function findMax(list){
+     
+      let max = list[0]
+     
+      for(i = 0; i < list.length; i++){
+       
+        if(list [i] > max){
+         
+          max = list[i];
+        } 
+      }
+      return max;
+    } 
+    function findMin(list){
+      
+      let min = list[0];
+
+      for(let i = 0; i < list.length; i++){
+        
+        if(list[i] < min){
+        
+          min = list[i];
+        }
+      return min
+      }
+     }
+
+
+    
+  }
+  function leech(){}
+
+  function seed(){}
+
+
+
+
+ 
 
     
 
@@ -32,4 +75,4 @@ function User(tracker, torrent){
 
 
 
-}
+
